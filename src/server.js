ae897864acf316b1a4701e849b1344b4faa42e0b
davinairs <davinairsanti@gmail.com>
2026-08-19 23:39:27 +0700

    fix: update port to process.env.PORT

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 const app = require("./app");
 const db = require("./config/database");
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 15390;
 
 async function testConnection() {
   try {
