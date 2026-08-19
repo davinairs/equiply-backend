834805a96bf4b6f355bbd87f584baad46c3fd507
davinairs <davinairsanti@gmail.com>
2026-08-20 00:17:28 +0700

    updated

--- src/config/database.js
+++ src/config/database.js
@@ -3,11 +3,11 @@ require("dotenv").config();
 const mysql = require("mysql2/promise");
 
 const db = mysql.createPool({
-  host: process.env.MYSQLHOST,
-  port: Number(process.env.MYSQLPORT),
-  user: process.env.MYSQLUSER,
-  password: process.env.MYSQLPASSWORD,
-  database: process.env.MYSQL_DATABASE,
+  host: process.env.DB_HOST || process.env.MYSQLHOST,
+  port: Number(process.env.DB_PORT || process.env.MYSQLPORT),
+  user: process.env.DB_USER || process.env.MYSQLUSER,
+  password: process.env.DB_PASSWORD || process.env.MYSQLPASSWORD,
+  database: process.env.DB_NAME || process.env.MYSQL_DATABASE,
   dateStrings: true,
 });
 
