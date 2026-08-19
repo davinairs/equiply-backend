418dd3196a9f5e90b7b569dd9afe6ddb0c70d960
davinairs <davinairsanti@gmail.com>
2026-08-20 00:38:05 +0700

    updated

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 const app = require("./app");
 const db = require("./config/database");
 
-const PORT = process.env.PORT || 15390;
+const PORT = process.env.PORT || 8080;
 
 async function testConnection() {
   try {
